533fced25ec5f951aeecd142e3ed2100b948ab17
takashun <s1832087PY@s.chibakoudai.jp>
2020-12-29 08:06:01 +0900

    BCDのWEB制作課題02のinit

---
+++ cit06-bcd/webseisaku02/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    let limit = 0;
+    let delta = 0;
+    while(true) {
+        if(limit === 5) {
+            break;
+        }
+        limit++;
+
+        moment.locale('ja');
+        const menu_day = moment().subtract(delta, 'week').day(1);
+        const img_url = 'https://www.cit-s.com/menu/' + menu_day.format('YYMMDD') + '_td.png'
+        const formatted_menu_day = menu_day.format('NNNNyoMMMDo');
+
+        let flag = 0;
+        $.ajax({
+            url: img_url,
+            type: 'GET',
+            cache: false,
+            async: false,
+        })
+        .done((data, textStatus, jqXHR) => {
+            flag = 1;
+
+            $('#menu-day').text(formatted_menu_day);
+            $('#lightbox-url').attr('href', img_url);
+            $('#img-tag').attr('src', img_url);
+        })
+        .fail((jqXHR, textStatus, errorThrown) => {
+            flag = 0;
+        })
+
+        if(flag === 1) {
+            break;
+        }
+        else if(flag === 0) {
+            delta++;
+        }
+    }
+});
